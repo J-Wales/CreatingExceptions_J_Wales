11b656dbed7e2f29aebea6745ed6d9e4a4e4b273
J-Wales <j.wales@edu.sait.ca>
2024-02-15 07:17:50 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CreatingExceptions_J_Wales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
